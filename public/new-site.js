7d5752969eb7d8f3879552adf1f3b6176eebfdf2
rigbone1 <1911734@rgu.ac.uk>
2020-05-21 04:00:14 +0100

    Added annotations.

--- public/new-site.js
+++ public/new-site.js
@@ -10,4 +10,20 @@ function setPage(n){
     // Do note, that the first screen is receiving the margin style, not the viewport itself.
     // Else, it falls out of view completely.
     document.getElementById('viewport').firstElementChild.style.marginLeft = `calc(-292px * ${n-1})`; // Subtract one to avoid an extra page shift.
+    focusAnnotations(n);
+}
+
+function focusAnnotations(n){
+    const noteLists = document.getElementsByClassName('annotations-list');
+    let screen;
+
+    for(let list of noteLists){
+        screen = +list.dataset.screen;
+
+        if(screen === n){
+            list.setAttribute('rel', 'focused');
+        } else if(screen !== n && list.getAttribute('rel')){
+            list.removeAttribute('rel');
+        }
+    }
 }
